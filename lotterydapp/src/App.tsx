8a70698324143b84540015fdbf759fe8092c1ada
Harshita <harshitalkadeep@gmail.com>
2024-03-22 20:18:22 +0530

    changes to improve UI

--- lotterydapp/src/App.tsx
+++ lotterydapp/src/App.tsx
@@ -1,4 +1,10 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { toast } from 'react-toastify';
 
 // Components
 import Navbar from "./components/Navbar";
@@ -35,9 +41,11 @@ const App: React.FC = () => {
     try {
       setLoading(true);
       await buyTicketOperation();
-      alert("brought a ticket");
+      toast.success("You have bought a ticket!");
+      setTimeout(() => window.location.reload(), 6000);
     } catch (error) {
-      throw error;
+      console.error("Error while buying a ticket:", error);
+      toast.error("Failed to buy ticket. Please try again.");
     }
     setLoading(false);
   };
@@ -47,41 +55,67 @@ const App: React.FC = () => {
     try {
       setLoading(true);
       await endGameOperation();
-      alert("end game");
+      toast.success("The game has ended!");
+      setTimeout(() => window.location.reload(), 6000);
     } catch (error) {
-      throw error;
+      console.error("Error while ending the game:", error);
+      toast.error("Failed to end game. Please try again.");
     }
     setLoading(false);
   };
 
   return (
-    <div className="h-100">
+    <div className="app-container">
       <Navbar />
-      <div className="d-flex flex-column justify-content-center align-items-center h-100">
+      <div className="main-container">
         {/* Ticket remaining display */}
-        <div className="py-1">Tickets remaining: {tickets}</div>
-        {/* Action Buttons */}
-        {tickets > 0 ? (
-          <button onClick={onBuyTicket} className="btn btn-primary btn-lg">
-            {/* TODO 7.b - Call onBuyTicket on click */}
-            {/* TODO 7.c - Show "loading..." when buying operation is pending */}
-            {loading ? "Loading . . ." : "Buy Ticket"}
-          </button>
-        ) : (
-            <button onClick={ onEndGame } className="btn btn-success btn-lg">
-            {/* TODO 11.b - Call onEndGame on click */}
-            {/* TODO 11.c - Show "loading..." when buying operation is pending */}
-            {loading ? "Loading . . ." : "End Game"}
-          </button>
-        )}
-        {/* List of Players */}
-        <div className="mt-2">
-          {Object.entries(players).map(([index, player]) => (
-            <div key={index}>
-              <b>Ticket {index}:</b> {player}
-            </div>
-          ))}
+        <div className="ticket-section">
+          <img
+            src="https://media.istockphoto.com/id/1332163793/vector/prize-raffle-rotating-drum-with-lottery-tickets-and-lucky-winner-gift-boxes-on-dark.jpg?s=612x612&w=0&k=20&c=PpeYywgzTgD-2OFJeSFQzQ08T5KBXAty5t5b92r70W0="
+            alt="Lottery Ticket"
+          />
+          <div className="ticket-info">Tickets remaining: {tickets}</div>
         </div>
+        {/* Action Buttons */}
+        <div className="action-buttons">
+          {tickets > 0 ? (
+            <button
+              onClick={onBuyTicket}
+              className="buy-ticket-btn btn-primary"
+            >
+              {/* TODO 7.b - Call onBuyTicket on click */}
+              {/* TODO 7.c - Show "loading..." when buying operation is pending */}
+              {loading ? "Loading . . ." : "Buy Ticket"}
+            </button>
+          ) : (
+            <button onClick={onEndGame} className="end-game-btn btn-success">
+              {/* TODO 11.b - Call onEndGame on click */}
+              {/* TODO 11.c - Show "loading..." when buying operation is pending */}
+              {loading ? "Loading . . ." : "End Game"}
+            </button>
+          )}
+        </div>
+        {/* List of Players */}
+        <div className="players-section">
+          <h3>Players:</h3>
+          <table className="players-table">
+            <thead>
+              <tr>
+                <th>S.No.</th>
+                <th>Player</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(players).map(([index, player]) => (
+                <tr key={index}>
+                  <td>{index}</td>
+                  <td>{player}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <ToastContainer position="bottom-right" draggable pauseOnHover/>
       </div>
     </div>
   );
